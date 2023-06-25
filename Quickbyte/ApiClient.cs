29a855e9a69b2815c17a42e7b25b6670bfec8814
Clément Habinshuti <habbes@Clements-MacBook-Pro.local>
2023-06-25 16:25:20 +0300

    Attempt to authentication from client

--- Quickbyte/ApiClient.cs
+++ Quickbyte/ApiClient.cs
@@ -9,18 +9,27 @@ public class ApiClient
 {
 	HttpClient client;
 
-	public ApiClient() : this(new HttpClient())
+	public ApiClient(string accessToken) : this(accessToken, new HttpClient())
 	{
 	}
 
-	public ApiClient(HttpClient httpClient)
+	public ApiClient(string accessToken, HttpClient httpClient)
 	{
 		this.client = httpClient;
+		this.client.BaseAddress = new Uri("http://localhost:3000/api");
+		this.client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+	}
+
+	public async Task<UserWithAccount> GetMe()
+	{
+		var response = await this.client.GetAsync("/me");
+		var result = await Serialize<UserWithAccount>(response);
+		return result;
 	}
 
 	public async Task<FileInitResponse> InitFile(string account, FileInitRequest request)
 	{
-		var requestUri = $"http://localhost:3000/accounts/{account}/files";
+		var requestUri = $"/accounts/{account}/files";
 		var body = JsonSerializer.Serialize(request,
 			new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }
 		);
@@ -34,5 +43,23 @@ public class ApiClient
 
 		return result;
 	}
+
+	public async Task<DownloadRequestResponse> RequestDownload(string accountId, string fileId)
+	{
+		var response = await this.client.GetAsync($"/accounts/{accountId}/files/{fileId}/download");
+		var result = await Serialize<DownloadRequestResponse>(response);
+		return result;
+	}
+
+	private async Task<T> Serialize<T>(HttpResponseMessage response)
+	{
+        var responseStream = await response.Content.ReadAsStreamAsync();
+        var result = JsonSerializer.Deserialize<T>(responseStream, new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        });
+
+		return result;
+    }
 }
 
--- Quickbyte/Models.cs
+++ Quickbyte/Models.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace Quickbyte;
 
 public record struct Block(int Index, string Id);
 
-public record FileInitResponse(string Container, string Path, string SecureUploadUrl, string OriginalName, string Provider, string Region);
+public record FileInitResponse(
+    [property: JsonPropertyName("_id")] string Id,
+    string Container, string Path, string SecureUploadUrl, string OriginalName, string Provider, string Region);
 
 public record FileInitRequest(
     string Provider, string Region, string OriginalName);
+
+public record UserWithAccount([property: JsonPropertyName("_id")] string Id, Account Account);
+public record Account([property: JsonPropertyName("_id")]  string Id);
+
+public record DownloadRequestResponse(
+    [property: JsonPropertyName("_id")] string Id,
+    string DownloadUrl);
