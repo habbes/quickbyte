3bc3cf523c9189a6aad1b482c0e8152c05d2f240
Clément Habinshuti <clhabins@microsoft.com>
2023-06-29 20:53:02 +0300

    Add new concurrent upload strategy

--- webapp/src/views/UploadView.vue
+++ webapp/src/views/UploadView.vue
@@ -178,12 +178,50 @@ function generateId(length: number) {
 }
 
 async function uploadBlockList(blob: BlockBlobClient, file: File, blockList: Block[], blockSize: number) {
+  // TODO: benchmark to decide between different upload strategies
+  // await uploadBlockListUsingSequentialBatches(blob, file, blockList, blockSize);
+  await uploadBlockListByIndependentWorkers(blob, file, blockList, blockSize);
+}
+
+async function uploadBlockListUsingSequentialBatches(blob: BlockBlobClient, file: File, blockList: Block[], blockSize: number) {
+  // In this strategy, uploads run in sequential batches
+  // Each batch performs n concurrent uploads and we wait to conclude
+  // a batch before starting the next one.
+  // The downside is that each batch takes as long as the slowest task.
+  // Other "workers" in the batch have to wait for the slowest one to complete
+  // before the next batch can begin.
+  // The upside is the relative simplicity of implementation
   const concurrency = 5;
   let current = 0;
   while (current < blockList.length) {
     const chunk = blockList.slice(current, current + concurrency);
     await Promise.all(chunk.map(block => uploadBlock(blob, file, block, blockSize)));
     current += concurrency;
+  }
+}
+
+async function uploadBlockListByIndependentWorkers(blob: BlockBlobClient, file: File, blockList: Block[], blockSize: number) {
+  // In this strategy, we have n workers
+  // Each worker is responsible for uploading every kn + workerIndex block
+  // e.g. if n = 5, worker 3 will upload blocks 3, 8, 13, etc.
+  // The benefit is that we don't need to maintain a queue or synchronization between workers,
+  // there's not contention between workers because there's no overlap
+  // The downside is that workers that finish all their work first will
+  // remain idle even if there's still work to be done
+  const concurrency = 5;
+  const workers = new Array(concurrency);
+  for (let i = 0; i < concurrency; i++) {
+    workers[i] = runUploadWorker(i, concurrency, blob, file, blockList, blockSize);
+  }
+
+  await Promise.all(workers);
+}
+
+async function runUploadWorker(workerIndex: number, totalWorkers: number, blob: BlockBlobClient, file: File, blockList: Block[], blockSize: number) {
+  let nextBlockIndex = workerIndex;
+  while (nextBlockIndex < blockList.length) {
+    await uploadBlock(blob, file, blockList[nextBlockIndex], blockSize);
+    nextBlockIndex += totalWorkers;
   }
 }
 
