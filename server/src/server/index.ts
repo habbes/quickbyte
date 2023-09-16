d8d19a2cf3bfd1735b90525d098cc3c49db98fbf
Clément Habinshuti <haby_habbes@live.com>
2023-09-16 23:24:40 +0300

    Add morgan for request logging

--- server/src/server/index.ts
+++ server/src/server/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import morgan from 'morgan';
 
 export function createServer() {
     const server = express();
+
+    server.use(morgan(('short')));
 
     server.get('/status', (req, res) => {
         res.status(200).json({ ok: true });
