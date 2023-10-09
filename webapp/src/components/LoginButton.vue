176710477d5739fde5cf3fda381373e3d9c73596
Clément Habinshuti <haby_habbes@live.com>
2023-10-09 03:00:25 +0300

    Demo video (#58)
    * Reduce logging from auth library
    
    * Add video demo on landing page

--- webapp/src/components/LoginButton.vue
+++ webapp/src/components/LoginButton.vue
@@ -1,13 +1,9 @@
 <template>
-  <div class="flex flex-col justify-center items-center">
-    <div>
-      <button type="button"
-        class="btn"
-        @click="auth.signIn()">
-        Sign in or create account
-      </button>
-    </div>
-  </div>
+  <button type="button"
+    class="btn w-full md:w-auto md:btn-lg"
+    @click="auth.signIn()">
+    Sign in or create account
+  </button>
 </template>
 <script setup lang="ts">
 import Button from "@/components/Button.vue";
