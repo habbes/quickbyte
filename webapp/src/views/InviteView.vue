08d274d10c66d44ec9e509ba0553102e8810b4cc
Clément Habinshuti <haby_habbes@live.com>
2023-12-31 00:00:09 +0300

    Some todo comments

--- webapp/src/views/InviteView.vue
+++ webapp/src/views/InviteView.vue
@@ -10,6 +10,14 @@ import { ensure } from '@/core';
 const route = useRoute();
 
 onMounted(async () => {
+  // todo when landing on invite view
+  // fetch/validate invite record
+  // if no logged in user:
+  //   show login/account page. After login prompt user to join project
+  // if logged in user different from invited email:
+  //    clear local cache, ask user to login, then prompt user to join project
+  // if logged in user same email
+  //     prompt user to join project
   const inviteId = ensure(route.params.inviteId) as string;
   await auth.signInWithInvite(inviteId);
 });
