6bbfd94386aa0c4755a81107225f0a2f61b2e5cb
Clément Habinshuti <haby_habbes@live.com>
2024-01-13 11:02:26 +0300

    Create coming soon pages for unimplemented views (#93)

---
+++ webapp/src/views/ProjectTasksView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-center items-center text-lg">
+    Coming soon.
+  </div>
+</template>
---
+++ webapp/src/views/ProjectTimelineView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center justify-center text-lg">
+    Coming soon.
+  </div>
+</template>
