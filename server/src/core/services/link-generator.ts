7d4fe41ec67a0b0e3423c3e47af9f6ad0d23ebd1
Clément Habinshuti <haby_habbes@live.com>
2024-01-24 22:17:10 +0300

    Create link generator service

---
+++ server/src/core/services/link-generator.ts
@@ -0,0 +1,22 @@
+
+export interface LinkGeneratorConfig {
+    webappBaseUrl: string;
+}
+
+export class LinkGenerator {
+    constructor(private config: LinkGeneratorConfig) {
+
+    }
+
+    getProjectUrl(projectId: string) {
+        return `${this.config.webappBaseUrl}/projects/${projectId}`;
+    }
+
+    getMediaUrl(projectId: string, mediaId: string) {
+        return `${this.getProjectUrl(projectId)}/player/${mediaId}`;
+    }
+
+    getMediaCommentUrl(projectId: string, mediaId: string, commentId: string) {
+        return `${this.getProjectUrl(projectId)}/player/${mediaId}?comment=${commentId}`
+    }
+}
