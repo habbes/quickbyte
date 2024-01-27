d3a0717e20dd3c28ed0351fbf5b0b391e5f26a6a
Clément Habinshuti <haby_habbes@live.com>
2024-01-27 22:49:44 +0300

    Make entire media card clickable

--- webapp/src/components/MediaCardItem.vue
+++ webapp/src/components/MediaCardItem.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="h-full w-full flex flex-col border border-[#615d6e] rounded-md">
-    <div class="flex-1 bg-black flex items-center justify-center">
-      <FilmIcon v-if="mediaType === 'video'" class="h-10 w-10"/>
-      <PhotoIcon v-else-if="mediaType === 'image'" class="h-10 w-10"/>
-      <MusicalNoteIcon v-else-if="mediaType === 'audio'" class="h-10 w-10"/>
-      <DocumentIcon v-else class="h-10 w-10"/>
-    </div>
-    <div class="h-12 bg-[#615d6e] flex flex-row items-center p-2 text-white">
-      <router-link :to="{ name: 'player', params: { projectId: media.projectId, mediaId: media._id } }">
-        {{ media.name }}
-      </router-link>
-    </div>
+    <router-link
+      :to="{ name: 'player', params: { projectId: media.projectId, mediaId: media._id } }"
+      class="h-full w-full flex flex-col"
+    >
+      <div class="flex-1 bg-black flex items-center justify-center">
+        <FilmIcon v-if="mediaType === 'video'" class="h-10 w-10"/>
+        <PhotoIcon v-else-if="mediaType === 'image'" class="h-10 w-10"/>
+        <MusicalNoteIcon v-else-if="mediaType === 'audio'" class="h-10 w-10"/>
+        <DocumentIcon v-else class="h-10 w-10"/>
+      </div>
+      <div class="h-12 bg-[#615d6e] flex flex-row items-center p-2 text-white">
+          {{ media.name }}
+      </div>
+    </router-link>
   </div>
 </template>
 <script lang="ts" setup>
