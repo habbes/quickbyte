b2c0b0dd82e8856194707b2e9ad31585522a5413
Clément Habinshuti <haby_habbes@live.com>
2024-01-27 09:05:10 +0300

    Adjust projects grid for mobile and add search

---
+++ webapp/src/components/ui/UiLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex" :class="classes">
+    <slot></slot>
+  </div>
+</template>
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const props = defineProps<{
+  fill?: boolean;
+  horizontal?: boolean;
+  itemsCenter?: boolean;
+  itemsBetween?: boolean;
+  justifyCenter?: boolean;
+  justifyBetween?: boolean;
+  justifyEnd?: boolean;
+  gap?: number;
+  gapSm?: boolean;
+  innerSpace?: boolean;
+  horizontalSpace?: boolean;
+  verticalScroll?: boolean;
+  fixedHeight?: string;
+}>();
+
+const classes = computed(() => {
+  return {
+    'flex-1': props.fill,
+    'flex-col': !props.horizontal,
+    'items-center': props.itemsCenter,
+    'items-between': props.itemsBetween,
+    'justify-center': props.justifyCenter,
+    'justify-between': props.justifyBetween,
+    [`gap-[${props.gap}]`]: props.gap,
+    'gap-2': props.gapSm,
+    'px-4 py-2': props.innerSpace,
+    'px-4': props.horizontalSpace,
+    'overflow-y-auto': props.verticalScroll,
+    [`h-[${props.fixedHeight}]`]: props.fixedHeight,
+  }
+})
+</script>
