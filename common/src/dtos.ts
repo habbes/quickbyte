a8310941744cd0ebbf8aebedf4a7418f74e47f5e
Clément Habinshuti <haby_habbes@live.com>
2024-01-30 22:37:54 +0300

    Add endpoint to rename or delete media

--- common/src/dtos.ts
+++ common/src/dtos.ts
@@ -8,9 +8,27 @@ export const DeclineInviteArgs = z.object({
 export type DeclineInviteArgs = z.infer<typeof DeclineInviteArgs>;
 
 export const AcceptInviteArgs = z.object({
-    code: z.string(),
-    name: z.string(),
-    email: z.string(),
+    code: z.string().min(1),
+    name: z.string().min(1),
+    email: z.string().email(),
 });
 
 export type AcceptInviteArgs = z.infer<typeof AcceptInviteArgs>;
+
+export const UpdateMediaArgs = z.object({
+    projectId: z.string().min(1),
+    id: z.string().min(1),
+    // since name is the only thing we can update at the moment,
+    // an update request must provide a name
+    name: z.string().min(1)
+});
+
+export type UpdateMediaArgs = z.infer<typeof UpdateMediaArgs>;
+
+export const DeleteMediaArgs = z.object({
+    projectId: z.string().min(1),
+    id: z.string().min(1)
+});
+
+export type DeleteMediaArgs = z.infer<typeof DeleteMediaArgs>;
+
