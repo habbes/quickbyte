da4dd27d3ed3b96d2f79db007ce1e25b4e7b3e01
Clément Habinshuti <haby_habbes@live.com>
2024-02-04 07:12:40 +0300

    Restrict toast width on large screens

--- webapp/src/components/Toast.vue
+++ webapp/src/components/Toast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="toast toast-top toast-center z-50 max-w-[100vw] w-full" v-if="toasts.length">
+  <div class="toast toast-top toast-center z-50 max-w-[100vw] w-full sm:max-w-[500px]" v-if="toasts.length">
     <div v-for="toast in toasts" :key="toast.id" :class="`alert alert-${toast.type} max-w-[100vw] whitespace-normal w-full`">
       {{ toast.message }}
     </div>
