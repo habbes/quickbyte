35fb2d1f3dda6589b0a3b671ceae8f9daf5b89ef
Clément Habinshuti <haby_habbes@live.com>
2024-02-24 17:26:15 +0300

    Create EmailAnnouncementService

---
+++ server/src/core/services/email-announcement-service.ts
@@ -0,0 +1,46 @@
+import { string } from "zod";
+import { Database } from "../db/index.js";
+import { createAppError, createResourceNotFoundError, rethrowIfAppError, createValidationError } from "../error.js";
+import { UserInDb, SendEmailAnnouncementArgs } from "../models.js";
+import { EmailHandler } from "./email/index.js";
+
+export interface EmailAnnouncementConfig {
+    email: EmailHandler;
+    secretKey: string;
+}
+
+export class EmailAnnouncementService {
+    constructor(private db: Database, private config: EmailAnnouncementConfig) {
+    }
+
+    public async sendAnnouncement(args: SendEmailAnnouncementArgs): Promise<void> {
+        try {
+            // this particular API is not exposed through trpc, so let's do
+            // the validation manually
+            const validationResult = SendEmailAnnouncementArgs.safeParse(args);
+            if (!validationResult.success) {
+                throw createValidationError(validationResult.error.message)
+            }
+
+            if (this.config.secretKey !== args.password) {
+                throw createResourceNotFoundError();
+            }
+
+            const users = await this.db.users().find<UserInDb>({}).toArray();
+            for (let user of users) {
+                // barebones templating :)
+                // Hopefully by the time we need something more robust, we
+                // won't be using this API for sending announcements :)
+                const message = args.message.replaceAll("{name}", user.name);
+                await this.config.email.sendEmail({
+                    to: user,
+                    subject: args.subject,
+                    message: message
+                });
+            }
+        } catch (e: any) {
+            rethrowIfAppError(e);
+            throw createAppError(e);
+        }
+    }
+}
