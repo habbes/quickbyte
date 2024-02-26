618cc156c6af216114dfc22cd27f3cb38221e7f6
Clément Habinshuti <haby_habbes@live.com>
2024-02-26 21:48:53 +0300

    Make toast closable

--- webapp/src/components/Toast.vue
+++ webapp/src/components/Toast.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="toast toast-top toast-center z-50 max-w-[100vw] w-full sm:max-w-[500px]" v-if="toasts.length">
-    <div v-for="toast in toasts" :key="toast.id" :class="`alert alert-${toast.type} max-w-[100vw] whitespace-normal w-full`">
-      {{ toast.message }}
+  <div
+    v-if="toasts.length"
+    class="toast toast-top toast-center z-50 max-w-[100vw] w-full sm:max-w-[500px]"
+  >
+    <div
+      v-for="toast in toasts"
+      :key="toast.id"
+      class="flex"
+      :class="`alert alert-${toast.type} max-w-[100vw] whitespace-normal w-full`"
+    >
+      <div class="flex-1">
+        {{ toast.message }}
+      </div>
+      <div>
+        <XCircleIcon @click="closeToast(toast.id)" class="w-5 h-5 cursor-pointer text-gray-700 hover:text-gray-900" />
+      </div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-import { toasts } from '@/app-utils';
+import { XCircleIcon  } from "@heroicons/vue/24/solid";
+import { toasts, closeToast } from '@/app-utils';
 </script>
