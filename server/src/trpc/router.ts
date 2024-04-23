1fdf53f6d934ff5a63c067b4dccdb55fa24a6333
Clément Habinshuti <haby_habbes@live.com>
2024-04-23 19:15:30 +0300

    Create API endpoints

--- server/src/trpc/router.ts
+++ server/src/trpc/router.ts
@@ -1,5 +1,17 @@
 import { router, publicProcedure, protectedProcedure } from './trpc.js';
-import { DeclineInviteArgs, AcceptInviteArgs, UpdateMediaArgs, CheckUserAuthMethodArgs, CreateUserArgs, VerifyUserEmailArgs, RequestUserVerificationEmailArgs, LoginRequestArgs, PasswordResetArgs, LoginWithGoogleRequestArgs, CreateMediaCommentArgs, DeleteMediaCommentArgs, UpdateMediaCommentArgs, InitTransferFileUploadArgs, CompleteFileUploadArgs, UpdateProjectArgs, ChangeProjectMemberRoleArgs, RemoveProjectMemberArgs, CreateFolderArgs, UpdateFolderArgs, GetProjectItemsArgs, CreateProjectMediaUploadArgs, CreateShareableTransferArgs, FinalizeTransferArgs, SearchProjectFolderArgs, DeleteProjectItemsArgs, MoveProjectItemsToFolderArgs, GetProjectMediaByIdArgs, InviteUsersToProjectArgs } from '@quickbyte/common';
+import {
+    DeclineInviteArgs, AcceptInviteArgs, UpdateMediaArgs,
+    CheckUserAuthMethodArgs, CreateUserArgs, VerifyUserEmailArgs,
+    RequestUserVerificationEmailArgs, LoginRequestArgs, PasswordResetArgs,
+    LoginWithGoogleRequestArgs, CreateMediaCommentArgs, DeleteMediaCommentArgs,
+    UpdateMediaCommentArgs, InitTransferFileUploadArgs, CompleteFileUploadArgs,
+    UpdateProjectArgs, ChangeProjectMemberRoleArgs, RemoveProjectMemberArgs,
+    CreateFolderArgs, UpdateFolderArgs, GetProjectItemsArgs, CreateProjectMediaUploadArgs,
+    CreateShareableTransferArgs, FinalizeTransferArgs, SearchProjectFolderArgs,
+    DeleteProjectItemsArgs, MoveProjectItemsToFolderArgs, GetProjectMediaByIdArgs,
+    InviteUsersToProjectArgs, CreateProjectShareArgs, UpdateProjectShareArgs,
+    DeleteProjetShareArgs
+} from '@quickbyte/common';
 import { z } from 'zod';
 
 export const appRouter = router({
@@ -151,6 +163,26 @@ export const appRouter = router({
     .input(CompleteFileUploadArgs)
     .mutation(({ input, ctx }) =>
         ctx.app.accounts.transfers(ctx.auth).completeFileUpload(input)),
+    
+    createProjectShare: protectedProcedure
+    .input(CreateProjectShareArgs)
+    .mutation(({ input, ctx }) =>
+        ctx.app.accounts.projects(ctx.auth).createProjectShare(input)),
+    
+    getProjectShares: protectedProcedure
+    .input(z.string())
+    .query(({ input, ctx }) =>
+        ctx.app.accounts.projects(ctx.auth).getProjectShares(input)),
+    
+    updateProjectShare: protectedProcedure
+    .input(UpdateProjectShareArgs)
+    .mutation(({ input, ctx }) =>
+        ctx.app.accounts.projects(ctx.auth).updateProjectShare(input)),
+    
+    deleteProjectShare: protectedProcedure
+    .input(DeleteProjetShareArgs)
+    .mutation(({ input, ctx }) =>
+        ctx.app.accounts.projects(ctx.auth).deleteProjectShare(input)),
     
     getUserAuthMethod: publicProcedure
     .input(CheckUserAuthMethodArgs)
