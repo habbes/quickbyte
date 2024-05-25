f3aab30e35a52d0a21e771240618fa80ffddaaa3
Clément Habinshuti <haby_habbes@live.com>
2024-05-25 22:30:58 +0300

    Ensure media filters are checked

--- server/src/core/globale-event-handler.ts
+++ server/src/core/globale-event-handler.ts
@@ -5,6 +5,7 @@ import { Media, Project, User } from "./models.js";
 import { BackgroundWorker } from "./background-worker.js";
 import { ensureSingleOrEmpty, wrapError } from "./utils.js";
 import { LinkGenerator } from '@quickbyte/common';
+import { addRequiredMediaFilters } from "./services/media-service.js";
 
 export class GlobalEventHandler {
     constructor(private config: GlobalEventHandlerConfig) {}
@@ -138,7 +139,7 @@ export class GlobalEventHandler {
             }
 
             // uploading a new versions of existing media
-            const media = await this.config.db.media().findOne<Media>({ _id: transfer.mediaId });
+            const media = await this.config.db.media().findOne<Media>(addRequiredMediaFilters({ _id: transfer.mediaId }));
             if (!media) {
                 throw createNotFoundError('media');
             }
