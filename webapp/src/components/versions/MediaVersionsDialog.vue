9336f1148100bc5025f0f21569292ff047928391
Clément Habinshuti <haby_habbes@live.com>
2024-05-25 16:17:57 +0300

    Create MediaVersionDialog shell

---
+++ webapp/src/components/versions/MediaVersionsDialog.vue
@@ -0,0 +1,29 @@
+<template>
+  <UiDialog
+    ref="dialog"
+    :title="`${media.name} versions`"
+  >
+  </UiDialog>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+import { UiDialog } from "@/components/ui";
+import type { Media } from "@quickbyte/common";
+
+defineProps<{
+  media: Media,
+  selectedVersionId: string;
+}>();
+
+defineExpose({ open, close });
+
+const dialog = ref<typeof UiDialog>();
+
+function open() {
+  dialog.value?.open();
+}
+
+function close() {
+  dialog.value?.close();
+}
+</script>
