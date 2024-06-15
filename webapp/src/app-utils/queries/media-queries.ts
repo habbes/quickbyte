1326480974e95db45e47e73b9462c1f4ac590932
Clément Habinshuti <haby_habbes@live.com>
2024-06-15 12:33:56 +0300

    Update project items query when media is updated

--- webapp/src/app-utils/queries/media-queries.ts
+++ webapp/src/app-utils/queries/media-queries.ts
@@ -1,7 +1,8 @@
-import { useQuery, QueryClient } from '@tanstack/vue-query'
+import { useQuery, QueryClient, useQueryClient } from '@tanstack/vue-query'
 import { trpcClient } from '../api';
 import type { MaybeRef, MaybeRefOrGetter } from 'vue';
-import { unref } from "vue";
+import { unref, watch } from "vue";
+import { upsertProjectItemsInQuery } from './project-items-queries';
 
 export function getMediaAssetQueryKey(projectId: MaybeRef<string>, mediaId?: MaybeRef<string|undefined>) {
     return ['media', projectId, mediaId];
@@ -13,13 +14,32 @@ export interface MediaAssetQueryOptions {
 
 export function useMediaAssetQuery(projectId: MaybeRef<string>, mediaId: MaybeRef<string>, opts?: MediaAssetQueryOptions) {
     const queryKey = getMediaAssetQueryKey(projectId, mediaId);
+    const client = useQueryClient();
     const result = useQuery({
         queryKey,
         queryFn: () => trpcClient.getProjectMediaById.query({
             projectId: unref(projectId),
             mediaId: unref(mediaId)
         }),
-        enabled: opts?.enabled
+        enabled: opts?.enabled,
+    });
+
+    // When media is re-fetched, update the corresponding entry
+    // in the project items collection so that it can go in sync
+    // immediately before the entire project items query needs to be refreshed
+    watch(result.data, (media) => {
+        if (!media) {
+            return;
+        }
+
+        upsertProjectItemsInQuery(client, projectId, media.folderId || undefined, {
+            _id: media._id,
+            type: 'media',
+            name: media.name,
+            _createdAt: media._createdAt,
+            _updatedAt: media._updatedAt,
+            item: media
+        });
     });
 
     return result;
