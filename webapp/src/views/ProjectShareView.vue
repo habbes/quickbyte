7bd15df399883cbf8bc8ffefe600b9ab45b571b0
Clément Habinshuti <haby_habbes@live.com>
2024-06-15 23:48:27 +0300

    Refactor fetching share items using tanstack query

--- webapp/src/views/ProjectShareView.vue
+++ webapp/src/views/ProjectShareView.vue
@@ -26,10 +26,10 @@
           </p>
         </UiLayout>
         <UiLayout fullWidth>
-          <form @submit.prevent="loadShare()">
+          <form @submit.prevent="submitPassword()">
             <UiLayout gapSm>
               <UiTextInput
-                v-model="password"
+                v-model="rawPasswordInput"
                 required
                 fullWidth
                 :type="'password'"
@@ -56,12 +56,12 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { ref, computed, watch, nextTick  } from "vue";
+import { ref, computed, watch  } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import { trpcClient, wrapError, projectShareStore, showToast, logger } from "@/app-utils";
-import { ensure } from "@/core";
-import type { GetProjectShareLinkItemsArgs, ProjectShareItemRef, ProjectShareLinkItemsSuccessResult } from "@quickbyte/common";
-import { getRemainingContentHeightCss, layoutDimensions } from "@/styles/dimentions.js";
+import { projectShareStore, showToast, logger, useProjectShareItemsQuery } from "@/app-utils";
+import { unwrapSingleton, unwrapSingletonOrUndefined } from "@/core";
+import type { ProjectShareLinkItemsSuccessResult } from "@quickbyte/common";
+import { layoutDimensions } from "@/styles/dimentions.js";
 import { UiLayout, UiTextInput, UiButton } from "@/components/ui";
 import NavBarBase from '@/components/NavBarBase.vue';
 import Logo from '@/components/Logo.vue';
@@ -69,108 +69,88 @@ import { ProjectShareDownloadAllButton } from "@/components/project-share";
 
 const route = useRoute();
 const router = useRouter();
-const code = computed(() => ensure(route.params.code as string));
-const loading = ref(false);
+
+const code = computed(() => unwrapSingleton(route.params.code));
+const shareId = computed(() => unwrapSingleton(route.params.shareId));
+const folderId = computed(() => unwrapSingletonOrUndefined(route.params.folderId));
+
 const share = ref<ProjectShareLinkItemsSuccessResult>();
+const rawPasswordInput = ref<string>();
 const password = ref<string>();
 const passwordRequired = ref(false);
 const headerHeight = layoutDimensions.projectHeaderHeight;
 const contentOffset = headerHeight + layoutDimensions.navBarHeight + layoutDimensions.projectHeaderHeight + 2;
-const contentHeight = getRemainingContentHeightCss(
-  contentOffset
-);
 
 const hiddenDownloader = ref<HTMLAnchorElement>();
 const currentDownloadUrl = ref<string>();
 
-function downloadItem(item: ProjectShareItemRef) {
-  if (item.type === 'folder') {
-    // TODO: support folder download
+const requestPassword = computed(() => (passwordRequired.value || undefined) && password.value);
+
+const queryEnabled = computed(() => passwordRequired.value ? !!password.value : true);
+
+const itemsQuery = useProjectShareItemsQuery({
+  shareId,
+  code,
+  folderId,
+  password: requestPassword
+}, {
+  enabled: queryEnabled,
+  // prevent retries because so that password errors can appear immediately.
+  // This feels hackish, we're using the query almost like a mutation
+  errorImmediately: true
+});
+
+const loading = computed(() => itemsQuery.isPending.value);
+
+watch(itemsQuery.data, (result) => {
+  if (!result) {
+    return;
+  }
+  if ('passwordRequired' in result) {
+    passwordRequired.value = result.passwordRequired;
+  } else {
+    share.value = result;
+    projectShareStore.setShare(share.value);
+    projectShareStore.setShareCode(code.value);
+    projectShareStore.setSharePassword(password.value);
+  }
+});
+
+watch(itemsQuery.error, (e: any) => {
+  if (!e) {
     return;
   }
 
-  if (!share.value) {
+  const data = e.data;
+  if (
+    (
+      data?.appCode === 'resourceNotFound' ||
+      data?.appCode === 'permissionError' ||
+      data?.httpStatus === 404 ||
+      data?.httpStatus === 403
+    ) && route.query.closePlayer
+  ) {
+    // since we were redirected to this page by closing the player
+    // instead of the user manually entering a url to a folder
+    // that's not shared, let's gracefully fall back by redirecting
+    // to the root of the share
+
+    router.push({
+      name: 'project-share',
+      params: {
+        shareId: share.value?._id, code: code.value
+      }
+    });
+
     return;
   }
 
-  const media = share.value.items.find(i => i._id === item._id && i.type === 'media');
-  if (!media || media.type !== 'media') {
-    return;
-  }
+  showToast(e.message, 'error');
+  logger.error(e.message, e);
+})
 
-  if (!media.item.file.downloadUrl) {
-    return;
-  }
-
-  currentDownloadUrl.value = media.item.file.downloadUrl;
-  nextTick(() => hiddenDownloader.value?.click());
+function submitPassword() {
+  password.value = rawPasswordInput.value;
 }
 
-async function loadShare() {
-  const shareId = ensure(route.params.shareId as string);
-  const folderId = route.params.folderId as string|undefined;
-
-  loading.value = true;
-
-  try {
-    const args: GetProjectShareLinkItemsArgs = {
-      shareId: shareId,
-      code: code.value
-    };
-
-    if (folderId) {
-      args.folderId = folderId;
-    }
-
-    if (passwordRequired.value && password.value) {
-      args.password = password.value
-    }
-
-    const result = await trpcClient.getProjectShareItems.query(args);
-
-    if ('passwordRequired' in result) {
-      passwordRequired.value = result.passwordRequired;
-    } else {
-      share.value = result;
-      projectShareStore.setShare(share.value);
-      projectShareStore.setShareCode(code.value);
-      projectShareStore.setSharePassword(password.value);
-    }
-  }
-  catch (e: any) {
-    const data = e.data;
-    if (
-      (
-        data?.appCode === 'resourceNotFound' ||
-        data?.appCode === 'permissionError' ||
-        data?.httpStatus === 404 ||
-        data?.httpStatus === 403
-      ) && route.query.closePlayer
-    ) {
-      // since we were redirected to this page by closing the player
-      // instead of the user manually entering a url to a folder
-      // that's not shared, let's gracefully fall back by redirecting
-      // to the root of the share
-
-      router.push({
-        name: 'project-share',
-        params: {
-          shareId: share.value?._id, code: code.value
-        }
-      });
-
-      return;
-    }
-
-    showToast(e.message, 'error');
-    logger.error(e.message, e);
-  }
-  finally {
-    loading.value = false
-  };
-}
-
-watch([() => route.params.shareId, () => route.params.code, () => route.params.folderId], async () => {
-  await loadShare();
-}, { immediate: true });
 </script>
