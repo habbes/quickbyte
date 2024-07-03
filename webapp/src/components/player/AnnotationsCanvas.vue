9aba1866516a6fa1985a80e4560ef7f206b073c0
Clément Habinshuti <haby_habbes@live.com>
2024-07-03 23:06:00 +0300

    Brainstorm drawing on annotations canvas

--- webapp/src/components/player/AnnotationsCanvas.vue
+++ webapp/src/components/player/AnnotationsCanvas.vue
@@ -1,14 +1,25 @@
 <template>
-  <KonvaStage :config="configKonva">
-    <KonvaLayer>
+  <KonvaStage
+    ref="stage"
+    :config="configKonva"
+    @mousedown="handleStageMouseDown($event)"
+    @touchstart="handleStageMouseDown($event)"
+    @mousemove="handleStageMouseMove($event)"
+    @touchmove="handleStageMouseMove($event)"
+    @mouseup="handleStageMouseUp($event)"
+    @touchend="handleStageMouseUp($event)"
+  >
+    <KonvaLayer ref="layerWrapper">
       <KonvaCircle :config="configCircle"></KonvaCircle>
     </KonvaLayer>
   </KonvaStage>
 </template>
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
+import konva from 'konva';
 
-
+const stage = ref<{ getStage: () => konva.Stage }>();
+const layerWrapper = ref<{ getNode: () => konva.Layer }>();
 
 const props = defineProps<{
   height: number;
@@ -26,5 +37,51 @@ const configCircle = {
   fill: "red",
   stroke: "black",
   strokeWidth: 4
+}
+
+let lastLine: konva.Line;
+let isPaint = false;
+
+function handleStageMouseDown(e: konva.KonvaPointerEvent) {
+  if (!layerWrapper.value) {
+    return;
+  }
+
+  isPaint = true;
+  const s = e.target.getStage()!;
+  const pos = s.getPointerPosition()!;
+  
+  const layer = layerWrapper.value.getNode();
+
+  lastLine = new konva.Line({
+    stroke: '#df4b26',
+    strokeWidth: 5,
+    globalCompositeOperation: 'source-over',
+    // round cap for smoother lines
+    lineCap: 'round',
+    lineJoin: 'round',
+    // add point twice, so we have some drawings even on a simple click
+    points: [pos.x, pos.y, pos.x, pos.y],
+  });
+
+  layer.add(lastLine)
+  
+}
+
+function handleStageMouseMove(e: konva.KonvaPointerEvent) {
+  if (!isPaint) {
+    return;
+  }
+
+  // prevent scrolling on touch devices
+  e.evt.preventDefault();
+
+  const pos = e.target.getStage()!.getPointerPosition()!;
+  var newPoints = lastLine.points().concat([pos.x, pos.y]);
+  lastLine.points(newPoints);
+}
+
+function handleStageMouseUp(e: konva.KonvaPointerEvent) {
+  isPaint = false;
 }
 </script>
