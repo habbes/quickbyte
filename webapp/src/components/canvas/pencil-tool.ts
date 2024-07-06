ebc870ff106743046c3a57e0cf5fca8b1cbc26d5
Clément Habinshuti <haby_habbes@live.com>
2024-07-06 16:36:45 +0300

    Optimize lines by skipping some points

--- webapp/src/components/canvas/pencil-tool.ts
+++ webapp/src/components/canvas/pencil-tool.ts
@@ -4,6 +4,12 @@ import type { FrameAnnotationPath } from "@quickbyte/common";
 export class PencilTool implements CanvasDrawingTool {
     private shapeHandler?: ShapeUpdateHandler;
     private shape?: FrameAnnotationPath;
+    /**
+     * This flag is toggled to skip some of the points from added to the line.
+     * It helps reduce the storage size of the line without noticeably affecting
+     * the perceived smootheness.
+     */
+    private skipNextPoint: boolean = true;
 
     constructor(private config: PencilToolConfig, private shapeId: string) {
     }
@@ -15,8 +21,17 @@ export class PencilTool implements CanvasDrawingTool {
 
     handlePointerMove(event: CanvasPointerEvent) {
         const shape = this.initShape(event.pos);
+        // We skip every other point, effectively reducing
+        // the number of points by 50%, to improve efficiency
+        // of processing and storing lines.
+        if (this.skipNextPoint) {
+            this.skipNextPoint = false;
+            return;
+        }
+
         const newPoints = [...shape.points, event.pos.x, event.pos.y];
         this.shape!.points = newPoints;
+        this.skipNextPoint = true;
         this.notifyShapeUpdate();
     }
 
@@ -30,6 +45,8 @@ export class PencilTool implements CanvasDrawingTool {
             type: 'path',
             strokeColor: this.config.strokeColor,
             strokeWidth: this.config.strokeWidth,
+            // duplicate the position so it will be a valid line (point)
+            // even if no other point is added.
             points: [pos.x, pos.y, pos.x, pos.y],
         };
 
