fbdc978c75b669ded0493cbe1d3d1fcf667350d0
Clément Habinshuti <haby_habbes@live.com>
2024-07-06 22:32:36 +0300

    Create CanvasController to handler undo/redo

--- webapp/src/components/canvas/index.ts
+++ webapp/src/components/canvas/index.ts
@@ -1,3 +1,4 @@
 export { default as AnnotationsCanvas } from "./AnnotationsCanvas.vue";
 export { default as DrawingTools } from "./DrawingTools.vue";
 export type { DrawingToolConfig } from './types';
+export * from "./controller-provider.js";
