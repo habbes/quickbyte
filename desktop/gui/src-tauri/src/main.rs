75fd468538446173e5558cb10d2c5b1dc2c89cae
Clément Habinshuti <haby_habbes@live.com>
2024-08-19 13:19:01 +0300

    Send data from frontend to desktop backend

--- desktop/gui/src-tauri/src/main.rs
+++ desktop/gui/src-tauri/src/main.rs
@@ -1,14 +1,37 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct ShareDownloadFile {
+  #[serde(rename = "_id")] // The camel case rename removes even leading _
+  _id: String,
+  transfer_id: String,
+  name: String,
+  size: i64,
+  account_id: String,
+  download_url: String,
+  #[serde(rename = "_createdAt")]
+  _created_at: String,
+}
+
 #[tauri::command]
 fn greet(name: &str) -> String {
   format!("Hello, {}!", name)
 }
 
+#[tauri::command]
+fn download_share(files: Vec<ShareDownloadFile>) {
+  println!("Results received.");
+  println!("What {:?}", files);
+}
+
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![download_share])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- desktop/gui/src/views/DownloadLinkView.vue
+++ desktop/gui/src/views/DownloadLinkView.vue
@@ -17,9 +17,10 @@
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
+import { invoke } from "@tauri-apps/api";
 import { trpcClient } from "../app-utils/index.js";
 
-const link = ref<string>();
+const link = ref<string>('http://localhost:5173/share/oEmxse-TDmz84AFR5R4nQw/ZdEkHATyq4jfr8QvIZCG_Q');
 const log = ref('');
 
 async function fetchLink() {
@@ -27,25 +28,27 @@ async function fetchLink() {
 
     "http://localhost:5173/share/oEmxse-TDmz84AFR5R4nQw/ZdEkHATyq4jfr8QvIZCG_Q";
     const segments = link.value.split('/');
-    const shareId = segments.at(-2);
-    const code = segments.at(-1);
+    const shareId = segments.at(-2)!;
+    const code = segments.at(-1)!;
 
     console.log('sharedId', shareId);
     console.log('code', code);
     log.value = `shareId ${shareId}, code ${code}`;
-    const args = {
-      shareId,
-      code
-    };
+    // const args = {
+    //   shareId,
+    //   code
+    // };
     // const result = await trpcClient.getProjectShareItems.query(args);
     const result = await trpcClient.getAllProjectShareFilesForDownload.query({
       shareId,
       shareCode: code
     });
-    log.value = `Result\n${JSON.stringify(result, null, 2)}`;
+    log.value += `\nResult\n${JSON.stringify(result, null, 2)}`;
+
+    await invoke('download_share', { files: result.files });
   }
   catch (e: any) {
-    log.value = `Error ${e}`;
+    log.value += `\nError ${e}`;
   }
 }
 </script>
