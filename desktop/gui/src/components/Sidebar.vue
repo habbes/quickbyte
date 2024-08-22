7856bb9f023921c63d9f899d422ca0a2b5abd47b
Clément Habinshuti <haby_habbes@live.com>
2024-08-22 17:23:19 +0300

    Load project data in sidebar

--- desktop/gui/src/components/Sidebar.vue
+++ desktop/gui/src/components/Sidebar.vue
@@ -12,8 +12,17 @@
     >
       Login to view project
     </router-link>
-    <div class="flex-1">
-      
+    <div class="flex-1 overflow-y-auto">
+      <div v-for="account in groupedProjects" :key="account._id">
+        <div>
+          {{ account.name }}
+        </div>
+        <div>
+          <div v-for="project in account.projects" :key="project._id">
+            {{ project.name }}
+          </div>
+        </div>
+      </div>
     </div>
     <router-link
       class="flex items-center border-t-[0.5px] border-t-gray-700 hover:bg-[#2a1824] h-12 px-2 text-sm"
@@ -28,7 +37,30 @@
   </div>
 </template>
 <script lang="ts" setup>
+import { computed } from "vue";
 import { store } from "@/app-utils";
+import type { WithRole, Project } from '@quickbyte/common';
+
+type AccountWithProjects = {
+  _id: string;
+  name: string;
+  projects: WithRole<Project>[];
+};
 
 const user = store.user;
+
+const groupedProjects = computed<AccountWithProjects[]>(() => {
+  const result: AccountWithProjects[] = [];
+  for (const account of store.accounts.value) {
+    const projects = store.projects.value.filter(p => p.accountId === account._id);
+    result.push({
+      _id: account._id,
+      name: account.name,
+      projects
+    });
+  }
+
+  return result;
+});
+
 </script>
