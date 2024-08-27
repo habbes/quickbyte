09e38219a718ce2440613e71790467d92e5ad35d
Clément Habinshuti <haby_habbes@live.com>
2024-08-27 14:29:04 +0300

    Create upload_files backend command

--- desktop/gui/src-tauri/src/commands.rs
+++ desktop/gui/src-tauri/src/commands.rs
@@ -1,12 +1,19 @@
 use tauri::{command, State};
 use crate::core::request::Request;
-use crate::core::dtos::SharedLinkDownloadRequest;
+use crate::core::dtos::*;
 use crate::core::app_context::AppContext;
 
 #[command]
 pub async fn download_shared_link(context: State<'_, AppContext>, request: SharedLinkDownloadRequest) -> Result<(), ()>{
     println!("Shared link download command received.");
     context.requests.send(Request::DownloadSharedLink(request)).await;
+    Ok(())
+}
+
+#[command]
+pub async fn upload_files(context: State<'_, AppContext>, request: UploadFilesRequest) -> Result<(), ()>{
+    println!("Upload files command received.");
+    context.requests.send(Request::UploadFiles(request)).await;
     Ok(())
 }
 
