dd9810bdc5e0f4e468d7b779a40de93758374e75
Clément Habinshuti <haby_habbes@live.com>
2024-09-07 23:45:16 +0300

    Fix login with google button width

--- desktop/gui/src/views/LoginView.vue
+++ desktop/gui/src/views/LoginView.vue
@@ -29,7 +29,7 @@
           />
         </div>
         <UiButton
-          fullWidth
+          fill
           @click="login()"
           primary
           :loading="loading"
@@ -39,7 +39,7 @@
         </UiButton>
         <div>
           <UiButton
-            fullWidth
+            fill
             @click="loginWithGoogle()"
           >
             Sign in with Google
