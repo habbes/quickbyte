990f308c65863c19d5c3181452c79fee2c016075
Clément Habinshuti <haby_habbes@live.com>
2024-09-22 08:15:29 +0300

    Add for different file types

--- desktop/gui/src/components/ProjectItemIcon.vue
+++ desktop/gui/src/components/ProjectItemIcon.vue
@@ -1,16 +1,30 @@
 <template>
-  <template v-if="props.item.type === 'folder'">
-    <Icon icon="lucide:folder" />
-  </template>
-  <template v-else>
-    <Icon icon="lucide:file" />
-  </template>
+  <Icon :icon="icon" />
 </template>
 <script lang="ts" setup>
+import { computed } from "vue";
 import { Icon } from "@iconify/vue";
-import type { ProjectItem } from "@quickbyte/common";
+import { type ProjectItem, getMediaType } from "@quickbyte/common";
 
 const props = defineProps<{
   item: ProjectItem
 }>();
+
+const icon = computed(() => {
+  if (props.item.type === 'folder') {
+    return 'lucide:folder';
+  }
+
+  const mediaType = getMediaType(props.item.name);
+  switch (mediaType) {
+    case 'audio':
+      return 'lucide:file-audio';
+    case 'video':
+      return 'lucide:file-video';
+    case 'image':
+      return 'lucide:file-image';
+    default:
+      return 'lucide:file';
+  }
+})
 </script>
