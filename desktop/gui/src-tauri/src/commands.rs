a661b6686431f63a059a22973e9248cc85a5a496
Clément Habinshuti <haby_habbes@live.com>
2024-10-10 13:13:13 +0300

    Make app error serializable

--- desktop/gui/src-tauri/src/commands.rs
+++ desktop/gui/src-tauri/src/commands.rs
@@ -1,4 +1,5 @@
 use tauri::{command, State};
+use crate::core::error::AppError;
 use crate::core::request::{DownloadFilesRequest, Request};
 use crate::core::dtos::*;
 use crate::app_context::AppContext;
@@ -7,44 +8,46 @@ use crate::auth::auth_store;
 
 #[command]
 pub async fn download_shared_link(context: State<'_, AppContext>, request: SharedLinkDownloadRequest) -> Result<(), ()>{
-    println!("Shared link download command received.");
     context.requests.send(Request::DownloadFiles(DownloadFilesRequest::FromSharedLink(request))).await;
     Ok(())
 }
 
 #[command]
 pub async fn download_legacy_transfer_link(context: State<'_, AppContext>, request: LegacyTransferLinkDownloadRequest) -> Result<(), ()> {
-    println!("Legacy transfer link download command received.");
     context.requests.send(Request::DownloadFiles(DownloadFilesRequest::FromLegacyTransferLink(request))).await;
     Ok(())
 }
 
 #[command]
 pub async fn upload_files(context: State<'_, AppContext>, request: UploadFilesRequest) -> Result<(), ()>{
-    println!("Upload files command received.");
     context.requests.send(Request::UploadFiles(request)).await;
     Ok(())
 }
 
 #[command]
 pub async fn request_transfers(context: State<'_, AppContext>) -> Result<(), ()> {
-    println!("Request transfers command issued");
     context.requests.send(Request::GetTransfers).await;
     Ok(())
 }
 
 #[command]
-pub async fn get_file_sizes(files: Vec<String>) -> Result<Vec<GetFileSizeResponse>, ()> {
-    println!("Fetching file sizes");
+pub async fn get_file_sizes(files: Vec<String>) -> Result<Vec<GetFileSizeResponse>, AppError> {
     let mut result = Vec::with_capacity(files.len());
     // TODO: get the file sizes concurrently
     for f in files {
-        let metadata = tokio::fs::metadata(&f).await.unwrap();
-        let name = std::path::Path::new(&f).file_name().unwrap();
+        let metadata = tokio::fs::metadata(&f).await?;
+        let name = std::path::Path::new(&f)
+        .file_name()
+        .map_or_else(
+            || Err(AppError::General("invalid file path".into())),
+            |n| Ok(n)
+        )?;
 
         result.push(GetFileSizeResponse {
             path: f.clone(),
-            name: String::from(name.to_str().unwrap()),
+            name: String::from(
+                name.to_str().map_or_else(|| Err(AppError::General("invalid path name".into())), |n| Ok(n))?
+            ),
             size: metadata.len()
         });
     }
--- desktop/gui/src-tauri/src/core/error.rs
+++ desktop/gui/src-tauri/src/core/error.rs
@@ -1,12 +1,28 @@
 use tokio::sync::mpsc::error::SendError;
+// see: https://docs.rs/thiserror/latest/thiserror/
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum AppError {
+    #[error("internal error occurred: {0}")]
     Internal(String),
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error("{0}")]
+    General(String),
 }
 
 impl<T> From<SendError<T>> for AppError {
     fn from(value: SendError<T>) -> Self {
-        AppError::Internal(String::from("Failed to send message over channel."))
+        AppError::Internal(String::from("failed to send message over channel"))
+    }
+}
+
+impl serde::Serialize for AppError {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
     }
 }
