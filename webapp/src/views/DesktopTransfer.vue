01e68adc7ff6e66d5da0c5bd7d99b1690c0c2c43
Clément Habinshuti <haby_habbes@live.com>
2024-10-21 00:46:03 +0300

    Rudimentary webpage

---
+++ webapp/src/views/DesktopTransfer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-col fixed w-full min-h-screen text-[#9499ae] text-sm">
+  <NavBarBase>
+    <template #logo>
+      <Logo />
+    </template>
+    <template #right>
+      
+    </template>
+  </NavBarBase>
+  <div class="flex flex-col mx-auto w-[700px] gap-5">
+    <UiLayout class=" text-5xl font-bold text-gray-200 text-center mt-10" alignCenter>
+      Faster and more reliable transfers of large files directly
+      from your computer.
+    </UiLayout>
+    <UiLayout class="text-sm">
+      <p>
+        The <span class="font-bold">Quickbyte Transfer</span> app lets you download files from shared Quickbyte links or upload
+        files directly to your projects. You don't need a Quickbyte account to download shared links. Download
+        the Quickbyte Transfer app now for free.
+      </p>
+    </UiLayout>
+    <UiLayout class="gap-2">
+      <UiButton primary>Download for Mac (Apple Silicon)</UiButton>
+      <UiButton primary>Download for Mac (Intel)</UiButton>
+      <UiButton primary>Download for Windows</UiButton>
+    </UiLayout>
+  </div>
+</div>
+</template>
+<script lang="ts" setup>
+import  { UiLayout, UiButton } from "@/components/ui";
+import AppShell from '@/components/AppShell.vue'
+import NavBarBase from '@/components/NavBarBase.vue';
+import Logo from '@/components/Logo.vue';
+</script>
