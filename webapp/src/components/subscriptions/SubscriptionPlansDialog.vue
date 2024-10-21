37a614f572b0bf7204b79579097ad3209ca33ca8
Clément Habinshuti <haby_habbes@live.com>
2024-10-21 22:29:08 +0300

    Improve subscription dialog views

--- webapp/src/components/subscriptions/SubscriptionPlansDialog.vue
+++ webapp/src/components/subscriptions/SubscriptionPlansDialog.vue
@@ -4,7 +4,7 @@
     <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
     <div class="fixed inset-0 flex w-screen items-center justify-center p-4">
       
-      <DialogPanel class="w-full max-w-sm rounded bg-white">
+      <DialogPanel class=" max-h-[90%] w-fit flex flex-col align-middle overflow-y-auto gap-2 py-4 px-4 items-center rounded bg-white">
         <DialogTitle>Choose a plan</DialogTitle>
         <SubscriptionPlans />
       </DialogPanel>
